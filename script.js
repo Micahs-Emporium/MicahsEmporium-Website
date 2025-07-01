f6b4f56186976058fbc89dd99c6a406236ae5c3e
Micahs-Emporium <suze.slaats@gmail.com>
2025-07-01 20:14:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ const image = L.imageOverlay('map/your-map.jpg', bounds).addTo(map);
 map.fitBounds(bounds);
 
 const locations = [
-  { name: 'Loc1-CollectiveKitchen', coords: [1330, 580], file: 'locations/Loc1-CollectiveKitchen.html' },
-  { name: 'Loc1-PublicPantry', coords: [700, 200], file: 'locations/Loc1-PublicPantry.html' },
-  { name: 'Loc1-DiningHall', coords: [600,780], file: 'locations/Loc1-DiningHall.html' },
-  { name: 'Loc1-MasterBedroom', coords: [1350, 2000], file: 'locations/Loc1-MasterBedroom.html' },
-  { name: 'Loc1-KidsRooms', coords: [820, 2215], file: 'locations/Loc1-KidsRooms.html' }
+  { name: 'Loc1-CollectiveKitchen', coords: [1330, 180], file: 'locations/Loc1-CollectiveKitchen.html' },
+  { name: 'Loc1-PublicPantry', coords: [200, 900], file: 'locations/Loc1-PublicPantry.html' },
+  { name: 'Loc1-DiningHall', coords: [1200,880], file: 'locations/Loc1-DiningHall.html' },
+  { name: 'Loc1-MasterBedroom', coords: [650, 1800], file: 'locations/Loc1-MasterBedroom.html' },
+  { name: 'Loc1-KidsRooms', coords: [870, 2340], file: 'locations/Loc1-KidsRooms.html' }
 ];
 
 locations.forEach(loc => {
